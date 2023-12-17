75af24d0f3cf6521066d8a0460df6cd6d2f5e94c
Ankita <ankitatiwari847@gmail.com>
2023-12-17 09:08:31 -0800

    CRA and tailwind

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-3xl font-bold underline">
+          Netflix gpt
+    </div>
+  );
+}
+
+export default App;
